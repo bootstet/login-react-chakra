380b84c13951d031811a32a97e5204fd05a96a0b
p_youzcchen <chenyouzeng@192.168.0.108>
2021-02-10 12:13:06 +0800

    fix: 静态资源

---
+++ build/precache-manifest.e2d25994a3a75a3a3db1e427a4e83b8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7485b7a1d713965b8c859b01453b60ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1f30521ea58a690508",
+    "url": "/static/js/2.3ea6fa30.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.3ea6fa30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7143e5a350269384c693",
+    "url": "/static/js/main.d4b4d85e.chunk.js"
+  },
+  {
+    "revision": "c456b9ed822d85244519",
+    "url": "/static/js/runtime-main.ea4d8429.js"
+  }
+]);
